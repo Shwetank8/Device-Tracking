98beb84d2a9aca9d58e58b589b34dc13f641ce70
Shwetank8 <sinhashwetank74@gmail.com>
2024-09-11 16:24:24 +0530

    updates

--- server.js
+++ server.js
@@ -24,4 +24,7 @@ app.get('/',(req,res) => {
     res.render('index')
 })
 
-server.listen(8000);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
